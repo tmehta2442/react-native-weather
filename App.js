0f152b511c03639146c578cb4004a119fdec62bc
Tejas Mehta <tejas@theoomf.com>
2019-08-22 16:47:00 -0400

    component

--- App.js
+++ App.js
@@ -1,40 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, TextInput, View, KeyboardAvoidingView, Platform } from 'react-native';
+
+import SearchInput from './components/SearchInput.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={[styles.largeText, styles.textStyle]}>
-          San Francisco
-        </Text>
-        <Text style={[styles.smallText, styles.textStyle]}>
-          Light Cloud
-        </Text>
-        <Text style={[styles.largeText, styles.textStyle]}>
-          24&#176;
-        </Text>
-      </View>
-    );
-  }
+        render() {
+            return (
+                <KeyboardAvoidingView
+                    style={styles.container}
+                    behavior="padding"
+                >
+                <Text style={[styles.largeText, styles.textStyle]}>
+                  San Francisco
+                </Text>
+                <Text style={[styles.smallText, styles.textStyle]}>
+                  Light Cloud
+                </Text>
+                <Text style={[styles.largeText, styles.textStyle]}>
+                  24&#176;
+                </Text>
+
+                <SearchInput
+                    
+                />
+            </KeyboardAvoidingView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    textAlign: 'center',
-    fontFamily:
-      Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
-  },
-  largeText: {
-    fontSize: 44
-  },
-  smallText: {
-    fontSize: 18
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textStyle: {
+        textAlign: 'center',
+        ...Platform.select({
+          ios: {
+            fontFamily: 'AvenirNext-Regular'
+          },
+          android: {
+            fontFamily: 'Roboto'
+          }
+        })
+    },
+    largeText: {
+        fontSize: 44
+    },
+    smallText: {
+        fontSize: 18
+    },
+    textInput: {
+        backgroundColor: '#666',
+        color: 'white',
+        height: 40,
+        width: 300,
+        marginTop: 20,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
+        alignSelf: 'center'
+    }
 });
